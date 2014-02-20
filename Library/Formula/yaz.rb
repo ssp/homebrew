017d3c5d188bd3e3bfeca7995028b0c6f7ee55b2
Sven-S. Porst <ssp-web@earthlingsoft.net>
2014-02-20 12:55:47 +0100

    towards building yaz from Index Data’s head

--- Library/Formula/yaz.rb
+++ Library/Formula/yaz.rb
@@ -4,10 +4,15 @@ class Yaz < Formula
   homepage 'http://www.indexdata.com/yaz'
   url 'http://ftp.indexdata.dk/pub/yaz/yaz-5.0.14.tar.gz'
   sha1 'a599f37cbbb91703c934f0207a346dd3a1cf9a5a'
-
+  head 'git://git.indexdata.com/yaz.git'
+  
   depends_on 'pkg-config' => :build
+  depends_on 'gnutls' => :build
+  depends_on 'libmemcached' => :build
+  depends_on 'libgcrypt' => :build
 
   def install
+    system "./buildconf.sh" if build.head?
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
                           "--with-xml2"
