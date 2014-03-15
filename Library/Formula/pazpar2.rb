22aba9f617d4a4013d79c434f9434b83fc582b5b
Sven-S. Porst <ssp-web@earthlingsoft.net>
2014-03-15 15:25:05 +0100

    pazpar2 1.6.39 (attempt 2)
    Address @mikemcquaid’s remarks on pull request #27547:
    
    * add a test: (ask pazpar2 to test a trivial server
    	configuration file; to be honest I doubt this test
    	is much more significant than testing the version)
    * use double quotes instead of quotes for strings
    	(it would be helpful if the examples/documentation
    	just stated this »rule«)
    * use make install instead of bin.install (the binary
    	ends up in sbin this way)

--- Library/Formula/pazpar2.rb
+++ Library/Formula/pazpar2.rb
@@ -1,17 +1,31 @@
-require 'formula'
+require "formula"
 
 class Pazpar2 < Formula
-  homepage 'http://www.indexdata.com/pazpar2'
-  url 'http://ftp.indexdata.dk/pub/pazpar2/pazpar2-1.6.39.tar.gz'
-  sha1 '2be29efecc4f0ed5a6a300638b7ef6577d1e34e4'
+  homepage "http://www.indexdata.com/pazpar2"
+  url "http://ftp.indexdata.dk/pub/pazpar2/pazpar2-1.6.39.tar.gz"
+  sha1 "2be29efecc4f0ed5a6a300638b7ef6577d1e34e4"
 
-  depends_on 'pkg-config' => :build
-  depends_on 'yaz'
+  depends_on "pkg-config" => :build
+  depends_on "yaz"
 
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}"
-    system 'make'
-    bin.install 'src/pazpar2'
+    system "make install"
   end
+
+  test do
+    (testpath/"test-config.xml").write <<-TEST_CONFIG.undent
+    <?xml version="1.0" encoding="UTF-8"?>
+    <pazpar2 xmlns="http://www.indexdata.com/pazpar2/1.0">
+      <threads number="2"/>
+      <server>
+        <listen port="8004"/>
+      </server>
+    </pazpar2>
+    TEST_CONFIG
+    
+    system "#{sbin}/pazpar2 -t -f #{testpath}/test-config.xml"
+  end
+  
 end
