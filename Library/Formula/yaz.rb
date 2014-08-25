dd5fc5fa864e5b0836cc4ada2fa129a79caeb63f
Sven-S. Porst <ssp-web@earthlingsoft.net>
2014-08-25 13:06:52 +0200

    yaz: depend on icu4c, add --without-icu option
    as discussed in #31830
    
    * add test for yaz-icu when building with ICU

--- Library/Formula/yaz.rb
+++ Library/Formula/yaz.rb
@@ -16,6 +16,11 @@ class Yaz < Formula
   depends_on "pkg-config" => :build
   depends_on "gnutls" => :optional
 
+  option "without-icu", "Build without ICU support and dependency on icu4c"
+  if build.with? "icu"
+    depends_on "icu4c"
+  end
+
   def install
     system "./configure", "--disable-dependency-tracking",
                           "--prefix=#{prefix}",
@@ -23,15 +28,53 @@ class Yaz < Formula
     system "make install"
   end
 
-  # This test converts between MARC8, an obscure mostly-obsolete library
-  # text encoding supported by yaz-iconv, and UTF8.
   test do
+    # This test converts between MARC8, an obscure mostly-obsolete library
+    # text encoding supported by yaz-iconv, and UTF8.
     File.open("marc8.txt", "w") do |f|
       f.write "$1!0-!L,i$3i$si$Ki$Ai$O!+=(B"
     end
-
     result = `"#{bin}/yaz-iconv" -f marc8 -t utf8 marc8.txt`.chomp
     result.force_encoding(Encoding::UTF_8) if result.respond_to?(:force_encoding)
     assert_equal "世界こんにちは！", result
+
+    # Test ICU support if building with ICU by running yaz-icu
+    # with the example icu_chain from its man page.
+    if build.with? "icu"
+      # The input string should be transformed to be:
+      # * without control characters (tab)
+      # * split into tokens at word boundaries (including -)
+      # * without whitespace and Punctuation
+      # * xy transformed to z
+      # * lowercase
+      configuration = <<-EOS.undent
+        <?xml version="1.0" encoding="UTF-8"?>
+        <icu_chain locale="en">
+          <transform rule="[:Control:] Any-Remove"/>
+          <tokenize rule="w"/>
+          <transform rule="[[:WhiteSpace:][:Punctuation:]] Remove"/>
+          <transliterate rule="xy > z;"/>
+          <display/>
+          <casemap rule="l"/>
+        </icu_chain>
+      EOS
+
+      input = "yaz-ICU	xy!"
+      expectedresult = <<-EOS.undent
+        1 1 'yaz' 'yaz'
+        2 1 '' ''
+        3 1 'icuz' 'ICUz'
+        4 1 '' ''
+      EOS
+
+      configurationfile = (testpath/"icu-chain.xml")
+      File.open(configurationfile, "w") { |f| f.write(configuration) }
+      inputfile = (testpath/"icu-test.txt")
+      File.open(inputfile, "w") { |f| f.write(input) }
+
+      command = "#{bin}/yaz-icu -c #{configurationfile} #{inputfile}"
+      result = `#{command}`
+      assert_equal expectedresult, result
+    end
   end
 end
